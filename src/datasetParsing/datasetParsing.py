32a250449b808ac4be081b27a6d6b24a85e3d988
Théophile JR <theophilejeromerocher44@gmail.com>
2024-12-05 19:27:44 +0100

    feat(datasetParsing): Parse perefectly every chess state

--- src/datasetParsing/datasetParsing.py
+++ src/datasetParsing/datasetParsing.py
@@ -9,28 +9,57 @@
 import string
 import sys
 
-def parseFile(filePath: string):
+class chessSate():
+    def __init__(self):
+        self.chessPlate = []
+        self.move: tuple[int, int] = (-1, -1)
+        self.turn = ''
+        self.castlingrights = ""
+        self.targetsquare = ''
+        self.halfmoveclock = -1
+        self.fullmove = -1
+    def __str__(self):
+        return f"plate: {self.chessPlate} move: {self.move} turn: {self.turn} castlingrights: {self.castlingrights} targetsquare: {self.targetsquare} halfmoveclock: {self.halfmoveclock} fullmove: {self.fullmove}"
+
+def parseFile(filePath: str) -> list[chessSate]:
     file = open(filePath, "r")
+    if file is None:
+        return None
     lines = file.readlines()
     file.close()
+    ret : list[chessSate] = []
     for dataLine in lines:
-        chessPlate = []
-        move = (-1, -1)
-        parseLineFen(chessPlate, dataLine)
-        print("NEW LINE GETTING PARSED BRO: ")
-        print(chessPlate)
+        state = chessSate()
+        parseDataLine(state, dataLine)
+        ret.append(state)
+    return ret
 
-def parseLineFen(plate, line : string):
+
+def parseDataLine(state : chessSate, line : str):
     parsed = line.split(" ")
-    plateStr = parsed[0]
-    plateRows = plateStr.split("/")
+    plateStrFen = parsed[0]
+    state.turn = parsed[1]
+    state.castlingrights = parsed[2]
+    state.targetsquare = parsed[3]
+    state.halfmoveclock = parsed[4]
+    state.fullmove = parsed[5]
+    if (len(parsed) > 6):
+        state.move = (parsed[6], parsed[7])
+    processFenStr(plateStrFen, state.chessPlate)
+
+
+def processFenStr(line : str, plate):
+    plateRows : list[str] = line.split("/")
     for row in plateRows:
         newRow = []
         for char in row:
             if char.isnumeric():
-                    newRow.append('' * int(char))
+                    for i in range(int(char)):
+                        newRow.append('.')
             else:
                 newRow.append(char)
         plate.append(newRow)
 
-parseFile("test.txt")
+
+for data in parseFile("test.txt"):
+    print(data)
