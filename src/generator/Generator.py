fda9c48d65466dab3473bebdb8219ad1076810ad
Kylian TRANCHET <kylian.tranchet@epitech.eu>
2024-12-09 10:00:07 +0100

    fix: generation of the first layer in Generator

--- src/generator/Generator.py
+++ src/generator/Generator.py
@@ -1,29 +1,27 @@
-from src.generator.Perceptron import Perceptron
+from generator.PerceptronLite import PerceptronLite
 import json
 
 class Generator :
 
-    def __init__(self, nb_input_values : int, nb_layouts : int, hidden_list : list[int]) -> None:
-        self.neural_network : list[list[Perceptron]] = []
+    def __init__(self, nb_input_values : int, nb_layers : int, layers_content : list[int]) -> None:
+        self.neural_network : list[list[PerceptronLite]] = []
         self.nb_input_values : int = nb_input_values
-        self.nb_layouts : int = len(hidden_list) + 1
-        self.init_network(nb_input_values, nb_layouts, hidden_list)
+        self.nb_layouts : int = len(layers_content) + 1
+        self.init_network(nb_input_values, nb_layers, layers_content)
 
-    def init_network(self, nb_input_values : int, nb_input : int, hidden_list : list[int]) -> None:
-        self.neural_network.append([Perceptron(nb_input_values)])
-        for i in range(nb_input - 1):
-            self.neural_network[0].append(Perceptron(nb_input_values))
+    def init_network(self, nb_input_values : int, nb_layers : int, layers_content : list[int]) -> None:
 
-        nb_last_input : int = nb_input
-        layer_iterator : int = 1
-        for i in hidden_list:
-            self.neural_network.append([Perceptron(nb_last_input)])
-            for j in range(i - 1):
-                self.neural_network[layer_iterator].append(Perceptron(nb_last_input))
+        nb_last_input : int = nb_layers
+        layer_iterator : int = 0
+
+        for i in layers_content:
+            self.neural_network.append([PerceptronLite(nb_last_input)])
+            for _ in range(i - 1):
+                self.neural_network[layer_iterator].append(PerceptronLite(nb_last_input))
             nb_last_input = i
             layer_iterator += 1
 
-        self.neural_network.append([Perceptron(nb_last_input)])
+        self.neural_network.append([PerceptronLite(nb_last_input)])
 
     def save_network(self, filepath : str = "neural_network_1.nn") -> None:
         network_data = {
